66cf27bb4a4586b638a5f9424ee100f8791e8d3e
patrykcwirko <37484838+patrykcwirko@users.noreply.github.com>
2022-07-01 20:39:04 +0200

    Add different movement in water and in air

---
+++ BialJam2022/Assets/CODE/EnviromentScaner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentScaner : MonoBehaviour
+{
+    [SerializeField] private LayerMask waterMask;
+
+    private bool _inAir, _inWater;
+
+    private void Update()
+    {
+        _inWater = Physics2D.BoxCast(transform.position, Vector2.one * 0.5f, 0, Vector2.down, 0.3f, waterMask);
+        _inAir = !_inWater;
+    }
+
+    public bool CheckIsInWater()
+    {
+        return _inWater;
+    }
+
+    public bool CheckIsInAir()
+    {
+        return _inAir;
+    }
+
+}
--- BialJam2022/Assets/CODE/PlayerMovement.cs
+++ BialJam2022/Assets/CODE/PlayerMovement.cs
@@ -7,22 +7,34 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float rollForce = 1f;
     [SerializeField] private float jumpForce = 100f;
+    [SerializeField] private float swimSpeed = 10f;
     [SerializeField] LayerMask _groundCheckMask;
 
+    private EnviromentScaner _scaner;
     private InputGather _gather;
     private Rigidbody2D _rb;
-    bool _isGrounded, _inWater;
+    private bool _isGrounded,  _canStomp;
 
     private void Start()
     {
         _gather = GetComponent<InputGather>();
         _rb = GetComponent<Rigidbody2D>();
+        _scaner = GetComponent<EnviromentScaner>();
+
         _gather.keys.jump += Jump;
     }
 
     private void Jump()
     {
-        if(_gather.keys.movement.y != 0 && (_inWater||_isGrounded||_gather.keys.movement.y<0.5f))
+        if (!_isGrounded || _scaner.CheckIsInWater()) return;
+        //Stomp
+        if(_canStomp && _gather.keys.movement.y < -0.5f)
+        {
+            _rb.AddForce(Vector2.down * jumpForce, ForceMode2D.Impulse);
+            _canStomp = false;
+        }
+        //Jump
+        if(_gather.keys.movement.y != 0 && _gather.keys.movement.y<0.5f)
         {
             _rb.AddForce(_gather.keys.movement * jumpForce, ForceMode2D.Impulse);
         }
@@ -30,19 +42,37 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if(_gather.keys.movement.sqrMagnitude > 0.01f)
+        
+        PerformInAirAction();
+
+        PerformInWaterAction();
+
+    }
+
+    private void PerformInWaterAction()
+    {
+        if (_scaner.CheckIsInAir()) return;
+        //free Move
+        if (_gather.keys.movement.sqrMagnitude > 0.01f)
         {
-            Debug.Log(_gather.keys.movement * rollForce);
-            _rb.AddForceAtPosition(_gather.keys.movement * rollForce * Time.deltaTime, transform.position + (_inWater?Vector3.zero:(Vector3)(Vector2.up * 0.1f)));
+            _rb.AddForce(_gather.keys.movement * swimSpeed * Time.deltaTime);
         }
 
-        if(!_inWater)
-            _isGrounded = Physics2D.BoxCast(transform.position,Vector2.one*0.5f,0,Vector2.down,0.3f,_groundCheckMask);
+    }
 
-        /* if(_gather.keys.jumpButton && _gather.keys.movement.y != 0)
+    private void PerformInAirAction()
+    {
+        if (_scaner.CheckIsInWater()) return;
+        //roll
+        if (_gather.keys.movement.sqrMagnitude > 0.01f)
         {
-            _rb.AddForce(_gather.keys.movement * jumpForce, ForceMode2D.Impulse);
-        } */
+            Debug.Log(_gather.keys.movement * rollForce);
+            _rb.AddForceAtPosition(_gather.keys.movement * rollForce * Time.deltaTime, transform.position + (Vector3)(Vector2.up * 0.1f));
+        }
+
+        //ground chek
+        _isGrounded = Physics2D.BoxCast(transform.position, Vector2.one * 0.5f, 0, Vector2.down, 0.3f, _groundCheckMask);
+        _canStomp = !_isGrounded;
     }
 
     void OnDrawGizmosSelected()
