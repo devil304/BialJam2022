f4bbc4c6470db5a5906483453c0d1e5009a37022
patrykcwirko <sdark431@gmail.com>
2022-07-02 10:10:08 +0200

    Add Little duck

---
+++ BialJam2022/Assets/CODE/LittleDuck.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LittleDuck : MonoBehaviour
+{
+    private bool _collected;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player") && !_collected)
+        {
+            UnityEngine.Debug.Log("Collected");
+            _collected = true;
+        }
+    }
+}
